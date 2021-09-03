3f98f99a1aa8b746df379dd888c30eaf94cafee3
guyunxiang <guyunxiang32@gmail.com>
2021-09-03 11:06:28 +0800

    修复切换主题后, 所有 dom 事件绑定丢失问题

--- themes/hexo-theme-Claudia-master/source/js/theme.js
+++ themes/hexo-theme-Claudia-master/source/js/theme.js
@@ -1,53 +1,53 @@
-function initialTheme() {
-
-  var setTheme = function(theme) {
-
-    $('#theme-btn').removeClass('icon-icon_light');
-    $('#theme-btn').removeClass('icon-icon_dark');
-    $('#theme-btn').addClass('icon-icon_' + (['auto', 'light'].includes(theme) ? 'dark' : 'light'));
-
-    $("body > .body-container").removeClass('appearance-auto');
-    $("body > .body-container").removeClass('appearance-dark')
-    $("body > .body-container").removeClass('appearance-light')
-    $("body > .body-container").addClass('appearance-' + theme);
-
-    $('#logo-img').attr('src', '/images/logo-' + theme + '.png');
-
-    window.localStorage.setItem('theme', theme);
-  }
-
-  setTheme(window.localStorage.getItem('theme') || 'auto');
-
-  let buttonenabled = true;
-
-  $(document).on("click", "#theme-btn", function() {
-
-    var localTheme = window.localStorage.getItem('theme') || 'auto';
-    var theme = ['auto', 'light'].includes(localTheme) ? 'dark' : 'light';
-
-  	if(!buttonenabled) return;
-  	buttonenabled = false;
-
-  	$(".clip").html($("body > .body-container")[0].outerHTML);
-    $(".clip .body-container").removeClass("appearance-" + localTheme);
-    $(".clip .body-container").addClass("appearance-" + theme);
-
-    $('#theme-btn').removeClass('icon-icon_light');
-    $('#theme-btn').removeClass('icon-icon_dark');
-    $('#theme-btn').addClass('icon-icon_' + (['auto', 'light'].includes(theme) ? 'dark' : 'light'));
-
-    $('#logo-img').attr('src', '/images/logo-' + theme + '.png');
-
-
-    $(".clip").addClass("anim");
-
-  	setTimeout(function() {
-      $("body > .body-container").replaceWith($(".clip").html())
-  		$(".clip").html("").removeClass("anim");
-      setTheme(theme);
-      buttonenabled = true;
-  	}, 1500);
-
-  });
-
-}
+// function initialTheme() {
+//
+//   var setTheme = function(theme) {
+//
+//     $('#theme-btn').removeClass('icon-icon_light');
+//     $('#theme-btn').removeClass('icon-icon_dark');
+//     $('#theme-btn').addClass('icon-icon_' + (['auto', 'light'].includes(theme) ? 'dark' : 'light'));
+//
+//     $("body > .body-container").removeClass('appearance-auto');
+//     $("body > .body-container").removeClass('appearance-dark')
+//     $("body > .body-container").removeClass('appearance-light')
+//     $("body > .body-container").addClass('appearance-' + theme);
+//
+//     $('#logo-img').attr('src', '/images/logo-' + theme + '.png');
+//
+//     window.localStorage.setItem('theme', theme);
+//   }
+//
+//   setTheme(window.localStorage.getItem('theme') || 'auto');
+//
+//   let buttonenabled = true;
+//
+//   $(document).on("click", "#theme-btn", function() {
+//
+//     var localTheme = window.localStorage.getItem('theme') || 'auto';
+//     var theme = ['auto', 'light'].includes(localTheme) ? 'dark' : 'light';
+//
+//   	if(!buttonenabled) return;
+//   	buttonenabled = false;
+//
+//   	$(".clip").html($("body > .body-container")[0].outerHTML);
+//     $(".clip .body-container").removeClass("appearance-" + localTheme);
+//     $(".clip .body-container").addClass("appearance-" + theme);
+//
+//     $('#theme-btn').removeClass('icon-icon_light');
+//     $('#theme-btn').removeClass('icon-icon_dark');
+//     $('#theme-btn').addClass('icon-icon_' + (['auto', 'light'].includes(theme) ? 'dark' : 'light'));
+//
+//     $('#logo-img').attr('src', '/images/logo-' + theme + '.png');
+//
+//
+//     $(".clip").addClass("anim");
+//
+//   	setTimeout(function() {
+//       $("body > .body-container").replaceWith($(".clip").html())
+//   		$(".clip").html("").removeClass("anim");
+//       setTheme(theme);
+//       buttonenabled = true;
+//   	}, 1500);
+//
+//   });
+//
+// }
