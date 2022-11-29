afe1ff96c6c5465c9425e0097b645a444099da30
guyunxiang <guyunxiang32@gmail.com>
2022-11-29 14:46:03 +0800

    unregister sw.js

---
+++ themes/hexo-theme-Claudia-master/source/sw.js
@@ -0,0 +1,9 @@
+caches.keys()
+  .then(keys =>
+    Promise.all(
+      keys.map(async key => console.log("caches.delete", key, await caches.delete(key)))))
+  .then(async () => {
+    console.log("registration.unregister", await registration.unregister());
+  })
+  .then(() => console.log("DONE"))
+  .catch(console.error);
