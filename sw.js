48eb6695b412bae7b3d155814ed3f2f2094cd127
guyunxiang <guyunxiang32@gmail.com>
2026-02-28 07:33:56 +0000

    Site updated: 2026-02-28 07:33:41

---
+++ sw.js
@@ -0,0 +1,1 @@
+caches.keys().then(e=>Promise.all(e.map(async e=>console.log("caches.delete",e,await caches.delete(e))))).then(async()=>{console.log("registration.unregister",await registration.unregister())}).then(()=>console.log("DONE")).catch(console.error);
